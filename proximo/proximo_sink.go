0864f80652c5c7b0bc944ee47523a358b1d35ed2
Michal Bock <michal.bock@gmail.com>
2019-02-11 13:59:52 +0000

    Make proximo sink actually asynchronous.

--- proximo/proximo_sink.go
+++ proximo/proximo_sink.go
@@ -2,13 +2,24 @@ package proximo
 
 import (
 	"context"
+	"io"
 
-	proximoc "github.com/uw-labs/proximo/proximoc-go"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
+
+	"github.com/uw-labs/proximo/proximoc-go"
 	"github.com/uw-labs/substrate"
 )
 
 var (
 	_ substrate.AsyncMessageSink = (*asyncMessageSink)(nil)
+
+	errStreamClosed = errors.New("stream was closed")
 )
 
 type AsyncMessageSinkConfig struct {
@@ -19,43 +30,136 @@ type AsyncMessageSinkConfig struct {
 
 func NewAsyncMessageSink(config AsyncMessageSinkConfig) (substrate.AsyncMessageSink, error) {
 
-	client, err := proximoc.DialProducer(
-		context.Background(),
-		config.Broker,
-		config.Topic,
-	)
+	conn, err := grpc.Dial(config.Broker, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
 	sink := asyncMessageSink{
-		client: client,
-		Topic:  config.Topic,
+		conn:  conn,
+		topic: config.Topic,
 	}
 	return &sink, nil
 }
 
 type asyncMessageSink struct {
-	client *proximoc.ProducerConn
-	Topic  string
+	conn  *grpc.ClientConn
+	topic string
 }
 
 func (ams *asyncMessageSink) PublishMessages(ctx context.Context, acks chan<- substrate.Message, messages <-chan substrate.Message) (rerr error) {
 
-	for {
+	eg, ctx := errgroup.WithContext(ctx)
 
+	client := proximoc.NewMessageSinkClient(ams.conn)
+	stream, err := client.Publish(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to start publishing")
+	}
+
+	err = stream.Send(&proximoc.PublisherRequest{
+		StartRequest: &proximoc.StartPublishRequest{
+			Topic: ams.topic,
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "failed to set publish topic")
+	}
+
+	toAck := make(chan *ackMessage)
+	proximoAcks := make(chan string)
+
+	eg.Go(func() error {
+		return ams.sendMessagesToProximo(ctx, stream, messages, toAck)
+	})
+	eg.Go(func() error {
+		return ams.receiveAcksFromProximo(ctx, stream, proximoAcks)
+	})
+	eg.Go(func() error {
+		return ams.passAcksToUser(ctx, acks, toAck, proximoAcks)
+	})
+
+	if err = eg.Wait(); err != nil && err != errStreamClosed {
+		return err
+	}
+	return nil
+}
+
+type msgSendStream interface {
+	Send(*proximoc.PublisherRequest) error
+}
+
+func (ams *asyncMessageSink) sendMessagesToProximo(ctx context.Context, stream msgSendStream, messages <-chan substrate.Message, toAck chan<- *ackMessage) error {
+	for {
 		select {
-		case m := <-messages:
-			if err := ams.client.Produce(m.Data()); err != nil {
-				return err
-			}
-			select {
-			case acks <- m:
-			case <-ctx.Done():
-				return nil
-			}
 		case <-ctx.Done():
 			return nil
+		case msg := <-messages:
+			pMsg := &proximoc.Message{
+				Id:   uuid.Must(uuid.NewV4()).String(),
+				Data: msg.Data(),
+			}
+			select {
+			case <-ctx.Done():
+				return nil
+			case toAck <- &ackMessage{id: pMsg.Id, msg: msg}:
+			}
+			if err := stream.Send(&proximoc.PublisherRequest{Msg: pMsg}); err != nil {
+				if err == io.EOF || status.Code(err) == codes.Canceled {
+					return errStreamClosed
+				}
+				return errors.Wrap(err, "failed to send message to proximo")
+			}
+		}
+	}
+}
+
+type ackReceiverStream interface {
+	Recv() (*proximoc.Confirmation, error)
+}
+
+func (ams *asyncMessageSink) receiveAcksFromProximo(ctx context.Context, stream ackReceiverStream, proximoAcks chan<- string) error {
+	for {
+		conf, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF || status.Code(err) == codes.Canceled {
+				return errStreamClosed
+			}
+			return errors.Wrap(err, "failed to receive acknowledgement")
+		}
+		select {
+		case <-ctx.Done():
+			return nil
+		case proximoAcks <- conf.MsgID:
+		}
+	}
+}
+
+func (ams *asyncMessageSink) passAcksToUser(ctx context.Context, acks chan<- substrate.Message, toAck <-chan *ackMessage, proximoAcks <-chan string) error {
+	ackMap := make(map[string]substrate.Message)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case ack := <-toAck:
+			ackMap[ack.id] = ack.msg
+		case msgID := <-proximoAcks:
+			msg, ok := ackMap[msgID]
+			if !ok {
+				return errors.New("received unexpected message confirmation from proximo")
+			}
+			sent := false
+			for !sent {
+				select {
+				case <-ctx.Done():
+					return nil
+				case ack := <-toAck:
+					ackMap[ack.id] = ack.msg
+				case acks <- msg:
+					delete(ackMap, msgID)
+					sent = true
+				}
+			}
 		}
 	}
 }
@@ -67,5 +171,10 @@ func (ams *asyncMessageSink) Status() (*substrate.Status, error) {
 // Close implements the Close method of the substrate.AsyncMessageSink
 // interface.
 func (ams *asyncMessageSink) Close() error {
-	return ams.client.Close()
+	return ams.conn.Close()
+}
+
+type ackMessage struct {
+	id  string
+	msg substrate.Message
 }
