ef98ad75142ca2bce748d59b7bfacf29d7610301
Martin Garton <garton@gmail.com>
2019-11-04 10:34:24 +0000

    Add some tests for S3 URL handling
    This ensures we're doing the right thing with S3 URLs

--- freezer/freezer_url_test.go
+++ freezer/freezer_url_test.go
@@ -49,6 +49,30 @@ func TestFreezerSink(t *testing.T) {
 			},
 			expectedErr: nil,
 		},
+		{
+			name:  "simple-s3",
+			input: "freezer+s3://foo/1",
+			expected: AsyncMessageSinkConfig{
+				FreezerConfig: freezer.MessageSinkConfig{
+					CompressionType: freezer.CompressionTypeNone,
+					Path:            "/1",
+				},
+				StreamStore: &mockStore{"s3://foo"},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "everything-s3",
+			input: "freezer+s3://foo/bar2/baz/?sse=aes256&compression=snappy",
+			expected: AsyncMessageSinkConfig{
+				FreezerConfig: freezer.MessageSinkConfig{
+					CompressionType: freezer.CompressionTypeSnappy,
+					Path:            "/bar2/baz/",
+				},
+				StreamStore: &mockStore{"s3://foo?sse=AES256"},
+			},
+			expectedErr: nil,
+		},
 	}
 
 	for _, tst := range tests {
@@ -103,6 +127,32 @@ func TestFreezerSource(t *testing.T) {
 					PollPeriod:      10 * time.Second,
 				},
 				StreamStore: &mockStore{"file:///"},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "simple-s3",
+			input: "freezer+s3://foo/baz1/",
+			expected: AsyncMessageSourceConfig{
+				FreezerConfig: freezer.MessageSourceConfig{
+					CompressionType: freezer.CompressionTypeNone,
+					Path:            "/baz1/",
+					PollPeriod:      10 * time.Second,
+				},
+				StreamStore: &mockStore{"s3://foo"},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "everything-s3",
+			input: "freezer+s3://foo/baz3/?compression=snappy&sse=aes256",
+			expected: AsyncMessageSourceConfig{
+				FreezerConfig: freezer.MessageSourceConfig{
+					CompressionType: freezer.CompressionTypeSnappy,
+					Path:            "/baz3/",
+					PollPeriod:      10 * time.Second,
+				},
+				StreamStore: &mockStore{"s3://foo?sse=AES256"},
 			},
 			expectedErr: nil,
 		},
