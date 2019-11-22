5bfa2f1d8a791d245ba99824ad9c129d37e27325
Bruno Luiz <bsilva@utilitywarehouse.co.uk>
2019-11-22 17:13:50 +0000

    change comments

--- noop/noop_sink.go
+++ noop/noop_sink.go
@@ -14,7 +14,7 @@ func NewAsyncMessageSink() (substrate.AsyncMessageSink, error) {
 	return &AsyncMessageSink{}, nil
 }
 
-// PublishMessages implements message publshing wrapped in instrumentation
+// PublishMessages implements message publishing by only acknowledging the messages
 func (ams *AsyncMessageSink) PublishMessages(ctx context.Context, acks chan<- substrate.Message, messages <-chan substrate.Message) (rerr error) {
 	for {
 		select {
@@ -35,7 +35,7 @@ func (ams *AsyncMessageSink) Close() error {
 	return nil
 }
 
-// Status returns the status of this sink, or an error if the status could not be determined.
+// Status returns the status of this sink (always working)
 func (ams *AsyncMessageSink) Status() (*substrate.Status, error) {
 	return &substrate.Status{Working: true}, nil
 }
