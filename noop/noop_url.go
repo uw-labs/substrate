74c85e9aed81340d6521fa7ce6e660530a45f847
Bruno Luiz <bsilva@utilitywarehouse.co.uk>
2019-11-22 16:57:33 +0000

    register url

---
+++ noop/noop_url.go
@@ -0,0 +1,18 @@
+package noop
+
+import (
+	"net/url"
+
+	"github.com/uw-labs/substrate"
+	"github.com/uw-labs/substrate/suburl"
+)
+
+func init() {
+	suburl.RegisterSink("noop", newNoopSink)
+}
+
+func newNoopSink(u *url.URL) (substrate.AsyncMessageSink, error) {
+	return noopSinker()
+}
+
+var noopSinker = NewAsyncMessageSink
