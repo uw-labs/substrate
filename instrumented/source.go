b51ed87f9749356a4050248d836e3bd70673e1a7
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2019-12-04 12:15:16 +0200

    Initialise counter metrics
    Helps with the precision of prom queries.

--- instrumented/source.go
+++ instrumented/source.go
@@ -26,6 +26,8 @@ func NewAsyncMessageSource(source substrate.AsyncMessageSource, counterOpts prom
 			panic(err)
 		}
 	}
+	counter.WithLabelValues("error", topic).Add(0)
+	counter.WithLabelValues("success", topic).Add(0)
 
 	return &AsyncMessageSource{
 		impl:    source,
