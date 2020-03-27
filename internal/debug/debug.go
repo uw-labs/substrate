5990c3b63e6bb86282506416d175ebaac03a3720
Martin Garton <garton@gmail.com>
2020-03-27 15:36:04 +0000

    Some basic debugging utils
    Basic type and some boilerplate for logging message information without
    spewing out entire message contents.

---
+++ internal/debug/debug.go
@@ -0,0 +1,40 @@
+package debug
+
+import (
+	"encoding/base64"
+	"hash/fnv"
+	"log"
+
+	"github.com/uw-labs/proximo/proto"
+	"github.com/uw-labs/substrate"
+)
+
+func messageHash(data []byte) string {
+	f := fnv.New32()
+	f.Write(data)
+	sum := f.Sum(nil)
+	return base64.StdEncoding.EncodeToString(sum)
+
+}
+
+type Debugger struct {
+	Enabled bool
+}
+
+func (d *Debugger) Logf(s string, args ...interface{}) {
+	if !d.Enabled {
+		return
+	}
+	args1 := make([]interface{}, len(args))
+	for i, arg := range args {
+		switch a := arg.(type) {
+		case substrate.Message:
+			args1[i] = messageHash(a.Data())
+		case *proto.Message:
+			args1[i] = messageHash(a.Data)
+		default:
+			args1[i] = arg
+		}
+	}
+	log.Printf(s, args1...)
+}
