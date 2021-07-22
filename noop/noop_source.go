98ef597b080d365c5872009c591b40c41cd1bc28
Michal Bock <michal.bock@gmail.com>
2021-07-22 16:50:26 +0100

    Add noop source that blocks until the provided context is canceled.
    This is useful when we want to be able to easily disable consumption from one source.
    We can use the noop reader and the service will behave as if no messages were coming in.
    
    I have also updated the sink to return `ctx.Err()` instead of `nil` to make it match the
    rest of the implementations.

---
+++ noop/noop_source.go
@@ -0,0 +1,33 @@
+package noop
+
+import (
+	"context"
+
+	"github.com/uw-labs/substrate"
+)
+
+// asyncMessageSource does no operations on publish messages calls
+type asyncMessageSource struct {
+	closed chan struct{}
+}
+
+// NewAsyncMessageSource returns a new AsyncMessageSource
+func NewAsyncMessageSource() substrate.AsyncMessageSource {
+	return &asyncMessageSource{}
+}
+
+// ConsumeMessages implements message consumption by blocking until the provided context is cancelled.
+func (ams asyncMessageSource) ConsumeMessages(ctx context.Context, _ chan<- substrate.Message, _ <-chan substrate.Message) error {
+	<-ctx.Done()
+	return ctx.Err()
+}
+
+// Close closes the message source
+func (ams asyncMessageSource) Close() error {
+	return nil
+}
+
+// Status returns the status of this source (always working)
+func (ams asyncMessageSource) Status() (*substrate.Status, error) {
+	return &substrate.Status{Working: true}, nil
+}
